4b5d1fd38cf6cd3c75a406390e6ad33f44297828
felix <felixfotball99@gmail.com>
2020-03-19 11:52:17 +0100

    --> fikset litt på estetikken

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
         OppgEn1 oppgEn1 = new OppgEn1();
 
-        System.out.println("Oppgave1: ");
+        System.out.println("Oppgave 1.1: ");
 
         oppgEn1.sum(10);
 
-        System.out.println("\n" + "Oppgave2: ");
+        System.out.println("\n" + "Oppgave 1.2: ");
 
         OppgEn2 oppgEn2 = new OppgEn2();
 
