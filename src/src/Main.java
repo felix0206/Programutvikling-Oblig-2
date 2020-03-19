c6e978d11afbbbb507cd6a6e0f5e594e576c367f
jørgen <s331352@oslomet.no>
2020-03-19 19:32:27 +0100

    oppgave en3

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,11 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //oppgave EN3
+
         OppgEn1 oppgEn1 = new OppgEn1();
 
         System.out.println("Oppgave 1.1: ");
 
         oppgEn1.sum(10);
+
+        //oppgave EN2
 
         System.out.println("\n" + "Oppgave 1.2: ");
 
@@ -14,6 +18,13 @@ public class Main {
 
         System.out.println("Summen av base^exponent blir: " + oppgEn2.pow(2,3));
 
-    }
 
-}
+        //oppgave EN3
+
+        OppgEn3 oppgEn3 = new OppgEn3();
+
+        int[] arr = {1,2,3};
+        System.out.println(oppgEn3.baklengs(arr, 0, arr.length-1));
+        }
+
+        }
