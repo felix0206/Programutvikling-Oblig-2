26139c59b45459c85178d4b3a525a68bd8da9375
jørgen <s331352@oslomet.no>
2020-03-21 21:19:36 +0100

    oppgave to2

---
+++ src/src/OppgTo3.java
@@ -0,0 +1,11 @@
+import java.lang.Object;
+import java.util.AbstractCollection;
+import java.util.List;
+
+public class OppgTo3 {
+
+    public abstract class AbstractList<E> extends AbstractCollection<E> implements List<E>{
+
+    }
+
+}
