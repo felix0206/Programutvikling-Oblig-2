70cb5afa234851bac7476486d62a08bfa0312817
felix <felixfotball99@gmail.com>
2020-03-22 22:12:10 +0100

    --> Oppg 4

--- src/src/OppgaveFire/Controller.java
+++ src/src/OppgaveFire/Controller.java
@@ -4,9 +4,13 @@ package OppgaveFire;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.HTMLEditor;
 
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Controller {
@@ -20,6 +24,17 @@ public class Controller {
     @FXML
     private AnchorPane anchor;
 
+    @FXML
+    private TextField TxtFil, TxtType;
+
+    @FXML
+    private Button SaveBtn;
+
+    @FXML
+    private Label LblFeilmeld;
+
+    String numberRegex = "[0-9]+";
+
     public void LoadBtn(ActionEvent event) throws Exception {
 
         //Kopier og lim inn path fra TestTråd.txt inne i OppgaveeFire package.
@@ -30,4 +45,22 @@ public class Controller {
         htmlEditor.setHtmlText(load.call());
 
     }
+
+    public void SaveBtn(ActionEvent event) throws IOException {
+
+        if (TxtFil.getText().isEmpty() || TxtType.getText().isEmpty() || !TxtType.getText().matches(numberRegex)){
+            LblFeilmeld.setText("Skriv inn filnavn og version! Bare tall i version");
+        }else{
+            try{
+                FileWriter fileWriter = new FileWriter(TxtFil.getText() + "-v" + TxtType.getText() + ".txt");
+                fileWriter.append(htmlEditor.getHtmlText());
+                fileWriter.close();
+                LblFeilmeld.setText("Filen: " + TxtFil.getText() + "-v" + TxtType.getText() + ".txt" + " ble lagret");
+            }catch (Exception e){
+                LblFeilmeld.setText("Feil ved lagring");
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
